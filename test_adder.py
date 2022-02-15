baa03dae3723b8d89cc4628cc5b619df5f781028
Alex <alexrkoch@gmail.com>
2022-02-15 14:07:51 -0500

    set keyboard input in tests

--- test_adder.py
+++ test_adder.py
@@ -2,7 +2,9 @@ import pytest
 import adder
 
 def test_adder1():
+  set_keyboard_input(["3", "4"])
   assert adder.add_function(3, 4) == 7
 
 def test_adder2():
-  assert adder.add_function(3.5, 6) == 9.5
+  set_keyboard_input(["39", "6"])
+  assert adder.add_function(39, 6) == 45
